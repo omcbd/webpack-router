6d04fe0edea0f14b21c71dd71e01df13be857af9
omcbd <917915734@qq.com>
2016-09-17 23:16:25 +0800

    newbranch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Routes from './route.js'
-import About from './about.js'
-
-
-
-ReactDOM.render(<Routes />, document.getElementById('app'));
